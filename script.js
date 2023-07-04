37330f1a11b856361864cd7b192426490533578f
DESKTOP-0V1BAA9\HAYOUNG <ha02e@naver.com>
2023-07-04 21:00:17 +0900

    230704_버튼 클릭시 이미지 보여짐

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-const generateBtn = document.querySelector('#generateBtn')
+const generateBtn = document.querySelector('#generateBtn');
+const gridContainer = document.querySelector('#gridContainer');
 
 generateBtn.addEventListener('click', () => {
-    console.log('버튼을 눌렀음!!!')
-})
+    const img = document.createElement('img');  //img 태그 생성
+    img.src = 'http://picsum.photos/500?random=1';  // 이미지 URL 할당
+
+    gridContainer.appendChild(img);  //이미지를 그리드 컨테이너에 추가
+});
