ae88d5a2ca62d2c8993b3ac661c29a2a35fd778c
DESKTOP-0V1BAA9\HAYOUNG <ha02e@naver.com>
2023-07-04 21:18:53 +0900

    230704_이미지 랜덤하게 보여주기

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ const generateBtn = document.querySelector('#generateBtn');
 const gridContainer = document.querySelector('#gridContainer');
 
 generateBtn.addEventListener('click', () => {
+    const randomNum = Math.floor(Math.random()*1000) + 1;  //1부터 1000 사이 랜덤
+    const imgUrl = `http://picsum.photos/500?random=${randomNum}`;  //이미지 주소 랜덤하게
+
     const img = document.createElement('img');  //img 태그 생성
-    img.src = 'http://picsum.photos/500?random=1';  // 이미지 URL 할당
+    img.src = imgUrl;
 
     gridContainer.appendChild(img);  //이미지를 그리드 컨테이너에 추가
 });
